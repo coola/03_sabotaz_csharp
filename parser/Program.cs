e43b9a17df4abe1c919a624dffa01dfab6754274
coola <coola@o2.pl>
2015-03-29 21:35:54 +0200

    last commit :(

--- parser/Program.cs
+++ parser/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace parser
+﻿using System;
+using System.Linq;
+using Parse;
+
+namespace parser
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+
+            // Niestety w tym tygodniu udało mi się zamiplementować tylko prostą analizę leksykalną
+            // pozdrawiam serdecznie
+            // Michał Kuliński
+
+            if (args.Any())
+            {
+                var inputString = args[0];
+                try
+                {
+                    new Parser().Parse(inputString);
+                }
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine(e.Message);
+                    Console.Out.WriteLine(e.StackTrace);
+                }
+            }
+            else
+            {
+                Console.Out.WriteLine("Please provide file name");
+            }
         }
     }
 }
